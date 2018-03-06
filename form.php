77a900d8aecad6330c2d0dbb634a56ccfb43e2b6
Emi Chen <emimbchen@gmail.com>
2018-03-06 16:32:24 -0600

    created form _post

---
+++ form.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Welcome <?php echo $_POST["lastname"]; ?><br>
+<?php echo $_POST["firstname"]." ". $_POST["lastname"];?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>PHP Form</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <h2>PHP Form</h2>
+    <form action="form.php" method="post">
+First Name: <input type="text" name="firstname"><br>
+Last Name: <input type="text" name="lastname"><br>
+<input type="submit">
+</form>
+</body>
+</html>
